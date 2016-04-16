aae3830cfdfb6c08010d49e3da7f0a325c0baed5
EtiennePelletier <etpelletier93@hotmail.com>
2016-04-16 03:15:01 -0400

    Allow running two GMC on different Raspberry Pi 2s
    - Update use of topics and GPIO pins.
    - If one of the limit switches associated to the global_limit_sw gpio is pressed, the hardware callback is disabled until it is not pressed anymore, with delays of 100 ms for software debouncing (with self.rate.sleep), then reactivated.
    - Receiving a global_disable from ROS will also disable the callback.

--- scripts/global_motor_control.py
+++ scripts/global_motor_control.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
 
 # Imports
+import os
 import pigpio
 import rospy
-from std_msgs.msg import Int32, String
+from std_msgs.msg import Bool, String
+
 
 class GlobalMotorControl:
-
-    def __init__(self):
-        self.node_name = self.__class__.__name__
+    def __init__(self, ID):
+        self.node_name = self.__class__.__name__ + ID
         rospy.init_node(self.node_name, anonymous=True)
         self.rate = rospy.Rate(10)  # 10 Hz
 
         # ROS subscriptions
-        # Expected format of Global_GPIO is 0 or 1
-        self.subscriber = rospy.Subscriber('Global_GPIO', Int32, self.callback_gpio)
+        # Expected format of Global_Enable is True or False
+        self.subscriber = rospy.Subscriber('Global_Enable', Bool, self.callback_gpio)
 
         # ROS publishments
         self.pub_sw = rospy.Publisher('Global_SW', String, queue_size=10)
@@ -24,38 +25,54 @@ class GlobalMotorControl:
         self.global_limit_sw   = 18  # pin 12
 
         self.gpio = pigpio.pi()
-        # self.gpio2 = pigpio.pi('10.43.156.12', 8888)
         self.gpio.set_mode(self.global_enable_pin, pigpio.OUTPUT)
-        # self.gpio2.set_mode(self.global_enable_pin, pigpio.OUTPUT)
         self.gpio.write(self.global_enable_pin, pigpio.LOW)
-        # self.gpio2.write(self.global_enable_pin, pigpio.LOW)
         self.gpio.set_mode(self.global_limit_sw, pigpio.INPUT)
-        cb_sw = self.gpio.callback(self.global_enable_pin, pigpio.RISING_EDGE, \
-                                                               self.callback_sw)
+        self.cb_sw_active = False
 
     def __del__(self):
         if hasattr(self.__class__, 'gpio'):
-            print("Global enable pin = HIGH")
-            self.gpio.write(self.global_enable_pin, pigpio.HIGH)
+            self.gpio.write(self.global_enable_pin, pigpio.LOW)
 
     def callback_gpio(self, data):
-        if data.data == 0:
-            self.gpio.write(self.global_enable_pin, pigpio.LOW)
-        else:
+        if data.data:
             self.gpio.write(self.global_enable_pin, pigpio.HIGH)
+            print("Global chip enable")
+        else:
+            self.gpio.write(self.global_enable_pin, pigpio.LOW)
+            self.cb_sw.cancel()
+            self.cb_active = False
+            print("Global chip disable")
 
     def callback_sw(self):
-        self.gpio.write(self.global_enable_pin, pigpio.HIGH)
+        self.cb_sw.cancel()
+        self.gpio.write(self.global_enable_pin, pigpio.LOW)
         self.pub_sw.publish(self.node_name)
+        print("Cancelling global switch callback")
+        self.cb_active = False
 
     def listener(self):
-        rospy.spin()
+        try:
+            while not rospy.is_shutdown():
+                self.rate.sleep()
+                if not self.cb_active and self.gpio.read(self.global_limit_sw):
+                    self.cb_active = True
+                    print("Creating global switch callback")
+                    self.cb_sw = self.gpio.callback(self.global_limit_sw, \
+                                                    pigpio.FALLING_EDGE, \
+                                                    self.callback_sw)
+
+        finally:
+            del self
 
 
 # Main function
 if __name__ == '__main__':
+    ID = os.getenv('ROS_HOSTNAME')
+    ID = ID.split(".")[-1] if ID else ""
+
     try:
-        gmc = GlobalMotorControl()
+        gmc = GlobalMotorControl(ID)
         gmc.listener()
 
     except rospy.ROSInterruptException as e:
